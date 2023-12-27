10e83bc90aaeb263d3573126376b58e258617494
Majd Jaber <majd.jaber@hp.com>
2023-12-27 19:54:57 +0200

    sends post api to backend to change voted or not, by firstname not id currently

--- src/components/VotersList.js
+++ src/components/VotersList.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import Statistics from "./Statistics";
+import axios from "axios";
 import "./styles/VotersList.css";
 
 const VotersList = ({ users: initialUsers, updateUserList }) => {
@@ -11,24 +12,34 @@ const VotersList = ({ users: initialUsers, updateUserList }) => {
   const [votedOnly, setVotedOnly] = useState(false);
   const [notVotedOnly, setNotVotedOnly] = useState(false);
 
+  // Use an object to store individual dropdown values for each user
+  const [dropdownValues, setDropdownValues] = useState({});
+
   useEffect(() => {
     setOriginalUsers(originalUsers);
     setFilteredUsers(filteredUsers);
+
+    // Initialize dropdown values based on initialUsers
+    const initialDropdownValues = {};
+    initialUsers.forEach((user) => {
+      initialDropdownValues[user.first_name] = user.voted; //TODO: user.id not user.first_name
+    });
+    setDropdownValues(initialDropdownValues);
   }, [initialUsers]);
 
   const handleSearch = (inputTerm, inputVotedOnly, inputNotVotedOnly, selectedBallot) => {
     const newSearchTerm = inputTerm.trim().toLowerCase();
     const newVotedOnly = inputVotedOnly;
     const newNotVotedOnly = inputNotVotedOnly;
-  
+
     const filtered = originalUsers.filter((user) => {
       const matchesSearchTerm =
         user.first_name.toLowerCase().includes(newSearchTerm) ||
         user.last_name.toLowerCase().includes(newSearchTerm) ||
         (user.id && user.id.includes(newSearchTerm));
-  
+
       const matchesBallotNumber = selectedBallot ? user.ballot === selectedBallot : true;
-  
+
       if (newVotedOnly && newNotVotedOnly) {
         return matchesSearchTerm && matchesBallotNumber;
       } else if (newVotedOnly) {
@@ -39,7 +50,7 @@ const VotersList = ({ users: initialUsers, updateUserList }) => {
         return matchesSearchTerm && matchesBallotNumber;
       }
     });
-  
+
     setSearchTerm(newSearchTerm);
     setVotedOnly(newVotedOnly);
     setNotVotedOnly(newNotVotedOnly);
@@ -48,13 +59,37 @@ const VotersList = ({ users: initialUsers, updateUserList }) => {
   };
 
   const handleClearSearch = () => {
-    console.log(initialUsers)
-    console.log(originalUsers)
     setSearchTerm("");
     setVotedOnly(false);
     setNotVotedOnly(false);
     setFilteredUsers(originalUsers);
     updateUserList(originalUsers);
+  };
+
+  const handleDropdownChange = (userId, userFirstName, voted) => { //TODO: userID here too
+    // Update the dropdown state for the specific user
+    setDropdownValues((prevValues) => ({
+      ...prevValues,
+      [userFirstName]: voted, //TODO: userID here too
+    }));
+  };
+
+  const handleSubmit = async (userId, userBallotNumber, userFirstName) => {
+    // Make a POST request to update the "voted" status
+    try {
+      const response = await axios.post("https://elections-bice.vercel.app/v1/elections/data", {
+        first_name: userFirstName, //TODO: dont want it
+        identity_number: userId==null? "208488692" : userId, // Replace with the actual identity_number
+        ballot_number: userBallotNumber, // Replace with the actual ballot_number
+        voted: dropdownValues[userFirstName], // TODO: again, by id, nit firstname
+      });
+
+      // Handle the response as needed
+      console.log("POST request successful", response.data);
+    } catch (error) {
+      // Handle errors
+      console.error("Error making POST request", error);
+    }
   };
 
   return (
@@ -90,7 +125,17 @@ const VotersList = ({ users: initialUsers, updateUserList }) => {
               <td>{user.street}</td>
               <td>{user.house_number}</td>
               <td>{user.ballot}</td>
-              <td>{user.voted ? "Yes" : "No"}</td>
+              <td>{user.voted ? <>&#x2713;</> : <>&times;</>}</td>
+              <td>
+                <select
+                  value={dropdownValues[user.first_name]} // TODO: user.id
+                  onChange={(e) => handleDropdownChange(user.id, user.first_name, e.target.value === 'true')}
+                >
+                  <option value={true}>Yes</option>
+                  <option value={false}>No</option>
+                </select>
+                <button onClick={() => handleSubmit(user.id, user.ballot, user.first_name)}>Submit</button>
+              </td>
             </tr>
           ))}
         </tbody>
